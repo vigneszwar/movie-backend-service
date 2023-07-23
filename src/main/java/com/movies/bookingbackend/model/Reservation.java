40eb3f86fb72c1a4d58e14f3b37c738a6495f0c7
Aswin <aswinmathi8@gmail.com>
2023-07-23 12:21:35 +0530

    reservation class

--- src/main/java/com/movies/bookingbackend/model/Reservation.java
+++ src/main/java/com/movies/bookingbackend/model/Reservation.java
@@ -1,4 +1,73 @@
 package com.movies.bookingbackend.model;
 
-public class Reservation {
+public class Reservation
+{
+    private String customerName;
+    private int totalTickets;
+    private double price;
+    private String timing;
+    private int seatsAvailable;
+
+    public Reservation(String customerName, int totalTickets, double price, String timing, int seatsAvailable)
+    {
+        this.customerName = customerName;
+        this.totalTickets = totalTickets;
+        this.price = price;
+        this.timing = timing;
+        this.seatsAvailable = seatsAvailable;
+    }
+    public String getCustomerName()
+    {
+        return customerName;
+    }
+    public void setCustomerName(String customerName)
+    {
+        this.customerName = customerName;
+    }
+    public int getTotalTickets()
+    {
+        return totalTickets;
+    }
+    public void setTotalTickets(int totalTickets)
+    {
+        this.totalTickets = totalTickets;
+    }
+    public double getPrice()
+    {
+        return price;
+    }
+    public void setPrice(double price)
+    {
+        this.price = price;
+    }
+    public String getTiming()
+    {
+        return timing;
+    }
+    public void setTiming(String timing)
+    {
+        this.timing = timing;
+    }
+    public int getSeatsAvailable()
+    {
+        return seatsAvailable;
+    }
+    public void setSeatsAvailable(int seatsAvailable)
+    {
+        this.seatsAvailable = seatsAvailable;
+    }
+    public void displayReservation()
+    {
+        System.out.println("Customer Name: " + customerName);
+        System.out.println("Total Tickets: " + totalTickets);
+        System.out.println("Price per Ticket: $" + price);
+        System.out.println("Timing: " + timing);
+        System.out.println("Seats Available: " + seatsAvailable);
+    }
+
+    public static void main(String[] args)
+    {
+        Reservation reservation = new Reservation("John Doe", 3, 10.5, "10:00 AM", 50);
+        reservation.displayReservation();
+    }
 }

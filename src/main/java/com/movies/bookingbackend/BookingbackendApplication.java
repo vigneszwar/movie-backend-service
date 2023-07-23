a3d22ae2206f7fa0c4fbfc133a0584842de0e02a
WOLFY <dkarthik0205@gmail.com>
2023-07-23 10:44:48 +0530

    first commit

---
+++ src/main/java/com/movies/bookingbackend/BookingbackendApplication.java
@@ -0,0 +1,13 @@
+package com.movies.bookingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movies/bookingbackend/BookingbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movies.bookingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
